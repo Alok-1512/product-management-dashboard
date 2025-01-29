47f67bb001615239c46e2aaf84f8401fd4195145
Alok Singh <alok15120803@gmail.com>
2025-01-30 02:21:58 +0530

    .

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+
+  return (
+    <>
+      <h2 className='text-xl text-center'>Product Management Dashboard</h2>
+      <div>
+      <Button>Click me</Button>
+    </div>
+    </>
+  )
+}
+
+export default App
